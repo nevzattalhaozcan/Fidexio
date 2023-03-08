fcea5a81b76460cb8bce8db9e7279264cdd8fa18
Nevzat Talha ÖZCAN <ozcann.talha@gmail.com>
2023-03-08 14:19:11 +0300

    Initial commit

---
+++ src/test/java/com/fidexio/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.fidexio.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/fidexio/step_definitions",
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+}
